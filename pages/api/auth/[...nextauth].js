cef3fe26d11e285ba27148b337d895d71530a99f
Mr-Shehroz <mr.shehroz.khan2006>
2025-04-12 01:15:27 +0500

    add

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,6 @@ import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import clientPromise from '../../../lib/db'
-const adminEmail = ['shehrozkhan.wow@gmail.com']
 
 export default NextAuth({
   providers: [
@@ -12,15 +11,4 @@ export default NextAuth({
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-  callbacks: {
-    session: ({token,session,user}) => {
-      console.log(session);
-      
-      if (adminEmail.includes(session?.user?.email)) {
-        return session
-      } else {
-        return false
-      }
-    }
-  }
 })
